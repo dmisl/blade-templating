3ed988ea4669dc9e08c60b2a00944b1dffbb7d96
unknown <rithlesscorp@gmail.com>
2025-05-21 02:54:58 +0200

    feat: create BasicController with index route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\BasicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BasicController::class, 'index'])->name('index');
